00ed7869cbc435c445b1582d2429bea59a9dff01
Andrew Sameh <andrewsameh22@hotmail.com>
2021-12-04 20:31:32 +0200

    Technology Section Added

---
+++ lib/modules/tech/Technology_screen.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, file_names
+
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_application/components.dart';
+import 'package:news_application/cubit/cubit.dart';
+import 'package:news_application/cubit/states.dart';
+
+class TechnologyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var list = NewsCubit.get(context).technology;
+        return articleBuilder(list, context);
+      },
+    );
+  }
+}
